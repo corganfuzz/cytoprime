54cfd90c8685b39ce1e32d2dc5326e5465e9279c
Ruiz, Gerald <gerald.ruiz@accenture.com>
2017-10-18 13:47:58 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Graph from './Graph';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Graph />
+      </div>
+    );
+  }
+}
+
+export default App;
